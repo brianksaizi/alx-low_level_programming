e78ccb2eaa70016c121a5eb360f6b90fcf77d934
root <saizbk@gmail.com>
2023-04-05 13:42:07 -0700

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,31 +1,53 @@
-i#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "../lists.h"
+#include "lists.h"
 
 /**
- * main - check the code for ALX School students.
+ * insert_nodeint_at_index - a function that inserts a new node at
+ *                           a given position
  *
- * Return: Always 0.
- */
-int main(void)
+ * @head: pointer to the first node of the list
+ * @idx: is the index of the list where the new node should be added
+ * @n: element to add to the new node
+ *
+ * Return: NULL if anything fails or the address of the new node
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *head;
+	listint_t *new_node, *current;
+	unsigned int index;
 
-	head = NULL;
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 2);
-	add_nodeint_end(&head, 3);
-	add_nodeint_end(&head, 4);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 402);
-	add_nodeint_end(&head, 1024);
-	print_listint(head);
-	printf("-----------------\n");
-	insert_nodeint_at_index(&head, 5, 4096);
-	insert_nodeint_at_index(&head, 9, 8090);
-	print_listint(head);
-	free_listint2(&head);
-	return (0);
+	current = *head; /*place first node at current*/
+
+	new_node = malloc(sizeof(listint_t));
+	if ((*head == NULL && idx != 0) || new_node == NULL)
+		return (NULL);
+
+	new_node->n = n; /* adds our element to the new node*/
+
+	/*iterates list to node position idx - 2*/
+	for (index = 0; head != NULL && index < idx - 1; index++)
+	{
+		current = current->next;
+		if (current == NULL)
+			return (NULL);
+	}
+
+	if (idx == 0) /*if the index for new node is 0*/
+	{
+		/*first node will be moved to second node*/
+		new_node->next = *head;
+		/*new node will be placed as the first node*/
+		*head = new_node;
+	}
+	else if (current->next) /*if index where to add our new node is not 0*/
+	{
+		new_node->next = current->next; /*places current node after new node*/
+		current->next = new_node;/*sets the new node at index idx*/
+	}
+	else /*if node position is not present in the list*/
+	{
+		new_node->next = NULL;/*sets next addr as NULL, indicates last node*/
+		current->next = new_node;/*sets the new node at the last position in list*/
+	}
+
+	return (new_node);
 }
