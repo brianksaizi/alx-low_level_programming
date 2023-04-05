2a36abcadea265e365decd481700e39893fc4535
root <saizbk@gmail.com>
2023-04-05 13:12:56 -0700

    update

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,35 +9,21 @@
 */
 size_t listint_len(const listint_t *h)
 {
-	/* Initialize a variable called 'elements' to 1,
-	 * which will count the number of elements in the list.
-	 * Start at 1 because we will count the first node
-	 * separately from the while loop below.
-	 */
 	size_t elements = 1;
 
-	/* If the pointer to the first node is NULL, the list is empty.
-	 * In that case, return 0.
-	 */
+	/*returns 0 as no of elements when h is NULL*/
 	if (h == NULL)
 		return (0);
 
-	/* While the pointer to the next node is not NULL,
-	 * indicating that there are still more nodes in the list:
-	 */
 	while (h->next != NULL)
 	{
-		/* If the integer value in the current node is not NULL
-		 * (indicated by the '\0' value), then increment 'elements'
-		 * to count this node.
-		 */
+		/*counts no of elements*/
 		if (h->n != '\0')
 			elements++;
 
-		/* Move the pointer to the next node in the list */
+		/*goes to next node*/
 		h = h->next;
 	}
 
-	/* Return the final count of elements in the list */
 	return (elements);
 }
