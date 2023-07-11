e6ae7b39320e4e5eec5a45e737faa117575a1524
briansaizi <saizibk@gmail.com>
2023-07-11 14:46:12 -0700

    Tread lightly, she is near

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,11 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <stdllib.h>
-#include <stdio.h>
-#include <elf.h>
+
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
