e52200968fbf37458be2812e8a5c7a09a922a148
brianksaizi <saizibk@gmail.com>
2023-08-03 18:58:51 -0700

    lastdance

--- 0x14-bit_manipulation/0-main.c
+++ 0x14-bit_manipulation/0-main.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main - Entry point to test the binary_to_uint function
+ * main - check the code
  *
  * Return: Always 0.
  */
@@ -10,25 +10,15 @@ int main(void)
 {
 	unsigned int n;
 
-	/* Test case: binary number "1" */
 	n = binary_to_uint("1");
 	printf("%u\n", n);
-
-	/* Test case: binary number "101" */
 	n = binary_to_uint("101");
 	printf("%u\n", n);
-
-	/* Test case: invalid binary number "1e01" */
 	n = binary_to_uint("1e01");
 	printf("%u\n", n);
-
-	/* Test case: binary number "1100010" */
 	n = binary_to_uint("1100010");
 	printf("%u\n", n);
-
-	/* Test case: binary number "0000000000000000000110010010" */
 	n = binary_to_uint("0000000000000000000110010010");
 	printf("%u\n", n);
-
 	return (0);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,24 +1,40 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdio.h>
-#include "main.h"
+#include <stdlib.h>
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    unsigned int n;
+/* Function prototype to print a character */
+int _putchar(char c);
 
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
-}
+/* Convert a binary number to an unsigned int. */
+unsigned int binary_to_uint(const char *b);
+
+/* Compute the exponential of a number. */
+unsigned int _pow(unsigned int a, unsigned int b);
+
+/* Get the length of a string. */
+int _strlen(const char *s);
+
+/* Print the binary representation of an unsigned long int. */
+void print_binary(unsigned long int n);
+
+/* Get the value of a bit at a given index. */
+int get_bit(unsigned long int n, unsigned int index);
+
+/* Set a bit at a given index to 1. */
+int set_bit(unsigned long int *n, unsigned int index);
+
+/* Set a bit at a given index to 0. */
+int clear_bit(unsigned long int *n, unsigned int index);
+
+/* Calculate the number of bits needed to flip to get from n to m. */
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+/* Convert an unsigned long int to its binary representation as a string. */
+char *to_binary(unsigned long int n);
+
+/* Check the endianness of the machine. */
+int get_endianness(void);
+
+#endif /* _MAIN_H_ */
