54721d962aa33fd061ff78b6d4f34212195923f9
brianksaizi <saizibk@gmail.com>
2023-08-03 19:25:35 -0700

    up

--- 0x14-bit_manipulation/1-main.c
+++ 0x14-bit_manipulation/1-main.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 #include "main.h"
-#include "limits.h"
 
 /**
- * main - Entry point to test the print_binary function
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	/* Test case: print binary representation of 0 */
 	print_binary(0);
 	printf("\n");
-
-	/* Test case: print binary representation of 1 */
 	print_binary(1);
 	printf("\n");
-
-	/* Test case: print binary representation of 98 */
 	print_binary(98);
 	printf("\n");
-
-	/* Test case: print binary representation of INT_MAX */
 	print_binary(INT_MAX);
 	printf("\n");
-
-	/* Test case: print binary representation of (1 << 10) + 1 */
 	print_binary((1 << 10) + 1);
 	printf("\n");
-
 	return (0);
 }
