14b07c4971f650afeeb97f2d627076547609e943
brianksaizi <saizibk@gmail.com>
2023-08-31 16:40:34 -0700

    lastdance

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int decimal_value = 0;
+	int i;
+	unsigned int decimal_value = 0;
 
-    if (!b)
-        return (0);
+	if (!b)
+		return (0);
 
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        decimal_value = 2 * decimal_value + (b[i] - '0');
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decimal_value = 2 * decimal_value + (b[i] - '0');
+	}
 
-    return (decimal_value);
+	return (decimal_value);
 }
