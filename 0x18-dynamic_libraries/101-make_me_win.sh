805b5ec61007ebafe82b70fa78663a8e00ccdef9
Brian <114440495+brianksaizi@users.noreply.github.com>
2023-09-01 00:53:19 +0200

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Load the LD_PRELOAD environment variable to use our shared library
-export LD_PRELOAD=./winning_hook.so
-
-# Run the gm program with your chosen numbers
-./gm 9 8 10 24 75 9
-
